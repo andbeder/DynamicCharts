d05589b62455efc15615903054e37d79ecf70de6
andbeder <andbeder@gmail.com>
2025-07-21 05:45:42 -0400

    Update test script and docs for current agents

--- test/packageScripts.test.js
+++ test/packageScripts.test.js
@@ -5,16 +5,14 @@ describe('package.json agent scripts', () => {
   test('defines npm scripts for each agent', () => {
     const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'));
     const scripts = pkg.scripts || {};
-    const agents = [
-      'sfdcAuthorizer',
-      'dashboardRetriever',
-      'dashboardReader',
-      'lwcReader',
-      'changeRequestGenerator',
-      'syncCharts',
-      'lwcTester',
-      'sfdcDeployer'
-    ];
+      const agents = [
+        'sfdcAuthorizer',
+        'dashboardRetriever',
+        'dashboardReader',
+        'lwcReader',
+        'lwcBuilder',
+        'sfdcDeployer'
+      ];
     agents.forEach((a) => expect(scripts).toHaveProperty(a));
     expect(scripts.dashboardRetriever).toMatch(/--dashboard-api-name=\$npm_config_dashboard/);
     expect(scripts.dashboardReader).toMatch(/--dashboard-api-name=\$npm_config_dashboard/);
