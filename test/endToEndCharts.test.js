3390fe51c69a1985d1a8579cfc116dda7f9f4614
andbeder <andbeder@gmail.com>
2025-07-22 17:08:20 -0400

    Remove deprecated agents

--- test/endToEndCharts.test.js
+++ test/endToEndCharts.test.js
@@ -1,19 +1,15 @@
 jest.mock('../scripts/agents/sfdcAuthorizer', () => jest.fn());
 jest.mock('../scripts/agents/dashboardRetriever', () => jest.fn());
 jest.mock('../scripts/agents/dashboardReader', () => jest.fn());
-jest.mock('../scripts/agents/lwcReader', () => jest.fn());
 jest.mock('../scripts/agents/changeRequestGenerator', () => jest.fn());
 jest.mock('../scripts/agents/syncCharts', () => jest.fn());
-jest.mock('../scripts/agents/lwcTester', () => jest.fn());
 jest.mock('../scripts/agents/sfdcDeployer', () => jest.fn());
 
 const sfdcAuthorizer = require('../scripts/agents/sfdcAuthorizer');
 const dashboardRetriever = require('../scripts/agents/dashboardRetriever');
 const dashboardReader = require('../scripts/agents/dashboardReader');
-const lwcReader = require('../scripts/agents/lwcReader');
 const changeRequestGenerator = require('../scripts/agents/changeRequestGenerator');
 const syncCharts = require('../scripts/agents/syncCharts');
-const lwcTester = require('../scripts/agents/lwcTester');
 const sfdcDeployer = require('../scripts/agents/sfdcDeployer');
 
 const runEndToEnd = require('../scripts/endToEndCharts');
@@ -35,10 +31,8 @@ describe('endToEndCharts workflow', () => {
       sfdcAuthorizer,
       dashboardRetriever,
       dashboardReader,
-      lwcReader,
       changeRequestGenerator,
       syncCharts,
-      lwcTester,
       sfdcDeployer,
     ].map(callOrder);
 
